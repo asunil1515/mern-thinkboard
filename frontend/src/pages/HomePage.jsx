37a57e6ec3d2fc0a2bca6ecab5ab395bcaef6f4e
asunil1515 <144961165+asunil1515@users.noreply.github.com>
2025-06-17 22:58:06 -0500

    Note functionality/UI
    implemented note creation, editing, and deletion functionality.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import Navbar from "../components/NavBar";
 import RateLimitedUI from "../components/RateLimitedUI";
 import { useEffect } from "react";
-import axios from "axios";
+import api from "../lib/axios";
 import toast from "react-hot-toast";
-import Note from "../../../backend/src/models/Note";
 import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -15,13 +15,13 @@ const HomePage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/notes");
+        const res = await api.get("/notes");
         console.log(res.data);
         setNotes(res.data);
         setIsRateLimited(false);
       } catch (error) {
         console.log("Error fetching notes");
-        console.log(error);
+        console.log(error.response);
         if (error.response?.status === 429) {
           setIsRateLimited(true);
         } else {
@@ -42,14 +42,14 @@ const HomePage = () => {
       {isRateLimited && <RateLimitedUI />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        {loading && (
-          <div className="text-center text-primary py-10">Loading notes...</div>
-        )}
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-              <NoteCard key={note.id} note={note} />
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
           </div>
         )}
